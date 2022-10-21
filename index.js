98baa6339ec333a75dad4a994d373297a937fe01
Pratik Talreja <talreja.p@northeastern.edu>
2022-10-20 21:52:40 -0400

    Integrated Sequelize

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const routes = require('./Routes')
 const express= require('express')
+const dotenv = require('dotenv');
+dotenv.config();
+const { Sequelize, Model, DataTypes } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.PGDATABASE, process.env.PGUSER, process.env.PGPASSWORD, {
+    host:process.env.PGHOST,
+    port: process.env.PGPORT,
+    dialect: 'postgres',
+    logging: false
+  })
+  
 
 
 const app=express();
@@ -15,5 +26,5 @@ const PORT = process.env.PORT || 3200;
 
 
 routes(app);
-app.listen(PORT, ()=>{
+app.listen(PORT,'127.0.1.0', ()=>{
 })
